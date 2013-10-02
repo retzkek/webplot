2b2b05a06f4b30526b8b1b5e026d3ef5be57f85e
Kevin Retzke <kevin.retzke@transware.net>
2013-10-01 22:12:43 -0500

    created root page as html/template

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"math"
 	"net/http"
 )
 
 const (
-	defaultAddress  = "localhost:8888"
+	defaultAddress  = "cobra:8888"
 	defaultPlotSize = 600
 )
 
@@ -16,7 +17,11 @@ type Root struct{}
 func (this Root) ServeHTTP(
 	w http.ResponseWriter,
 	r *http.Request) {
-	fmt.Fprint(w, "<h1>hello</h1><img src=\"/test.png\"/>")
+	t, _ := template.New("root").Parse(rootTemplate)
+	d := map[string]string{
+		"Title":     "webplot example",
+		"ImagePath": "/test.png"}
+	t.ExecuteTemplate(w, "root", d)
 }
 
 func main() {
@@ -25,10 +30,10 @@ func main() {
 	http.Handle("/", r)
 	var p Plot
 	p.setSize(defaultPlotSize, defaultPlotSize)
-	maxval := float64(defaultPlotSize * defaultPlotSize)
-	p.setFunc(func(x, y int) float64 {
-		return float64(x) * float64(y) / maxval
-	})
+	//maxval := float64(defaultPlotSize * defaultPlotSize)
+	//p.setFunc(func(x, y int) float64 {
+	//	return float64(x) * float64(y) / maxval
+	//})
 	p.setFunc(func(x, y int) float64 {
 		return math.Cos(float64(x)/25.0)*math.Sin(float64(y)/25.0)/2.0 + 0.5
 	})
@@ -36,3 +41,15 @@ func main() {
 	http.Handle("/test.png", p)
 	http.ListenAndServe(defaultAddress, nil)
 }
+
+var rootTemplate = `
+<html>
+<head>
+<title>{{.Title}}</title>
+</head>
+<body>
+<h1>{{.Title}}</h1>
+<img src="{{.ImagePath}}"/>
+</body>
+</html>
+`
